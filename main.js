532aef04aba96e52e27d32dd02cbd91c13a6e16d
Brooks Tiffany <brooks.t.tiffany@gmail.com>
2023-07-15 14:00:12 -0400

    catching up

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
 console.log("main.js is operational!");
+
+const screen = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".clear");
+const backspace = document.querySelector(".delete");
+const operator = document.querySelectorAll(".operator");
+const number = document.querySelectorAll(".number");
+
+let screenValue = "";
+
+buttons.forEach(function (button) {
+	button.addEventListener("click", handleClick);
+});
